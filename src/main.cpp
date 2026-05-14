269921be632b041721958f5a1a62a3e979f82450
Felipe Gama <felipegamae2@gmail.com>
2026-05-14 00:05:37 -0300

    making the error equals 0 and stop the model

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
+#include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
 
-void costFunction(float w) {
-  int trainingdata[][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
-
-  int trainingdata_size = sizeof(trainingdata) / sizeof(trainingdata[0]);
+float costFunction(float &w, float alpha, int trainingdata[][2], int size) {
   float distance = 0;
-  for (int i = 0; i < trainingdata_size; i++) {
+  float moment = 0;
+  float scale = 0;
+  float beta_1 = 0.9;
+  float beta_2 = 0.999;
+  float gradient;
+  int inputs;
+
+  for (int a = 0; a < size; ++a) {
+    inputs += trainingdata[a][0];
+  }
+
+  for (int i = 0; i < size; i++) {
     float model_output = trainingdata[i][0] * w;
     // We have to see the distance by the model output:
     cout << "Input: " << trainingdata[i][0]
@@ -19,13 +29,22 @@ void costFunction(float w) {
             "------------"
          << endl;
     distance += (model_output - trainingdata[i][1]);
+    gradient = 2 * trainingdata[i][0];
+    moment = beta_1 * moment + (1 - beta_1) * gradient;
+    scale = beta_2 * scale + (1 - beta_2) * (gradient * gradient);
   }
 
-  cout << "The Square Mean Error: " << (distance * distance) << "\n\n" << endl;
+  cout << "The Square Error: " << (distance * distance) << "\n\n" << endl;
+
+  w = w - (alpha / (sqrt(scale) + 1e-8)) * moment;
+
+  return distance * distance;
 }
 
 int main() {
   // For the first, we need the training data for the model:
+  static int trainingdata[][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
+  int trainingdata_size = sizeof(trainingdata) / sizeof(trainingdata[0]);
 
   // We know that our training data is determined by this equation:
   // y = x * w
@@ -39,6 +58,9 @@ int main() {
   // float w = (float)rand() / RAND_MAX;
 
   float w = 60.0;
+  float alpha = 0.1;
+  float error;
+  float final_w;
 
   // Compare the output with the rand parameter w:
 
@@ -47,11 +69,36 @@ int main() {
   // What if minimizating the w a little:
   float e = 1e-3;
 
-  for (int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 2515; ++i) {
+    cout << "Interation " << i + 1 << endl;
     cout << "w: " << w << endl;
-    costFunction(w);
-    w -= e;
+    final_w = w;
+    error = costFunction(w, alpha, trainingdata, trainingdata_size);
+
+    if (error < 10000 && error > 1000) {
+      alpha = 0.01;
+    } else if (error < 1000 && error > 100) {
+      alpha = 0.001;
+    } else if (error < 100 && error > 1e-6) {
+      alpha = 0.0001;
+    } else if (error < 1e-6) {
+      auto old_flags = cout.flags();
+      auto old_precision = cout.precision();
+
+      cout << "\nThe model learned!!! The w is " << fixed << setprecision(1)
+           << final_w << endl;
+
+      cout.flags(old_flags);
+      cout.precision(old_precision);
+      break;
+    }
   }
+
+  auto old_flags = cout.flags();
+  auto old_precision = cout.precision();
+
+  cout.flags(old_flags);
+  cout.precision(old_precision);
 
   return 0;
 }
